71941ffea2bb0f7dccdde65850a4597e84094ecb
Loïc TRUCHOT <loic.truchot.pro@gmail.com>
2022-12-19 18:05:47 +0000

    :recycle: refactor(main): fixed with Malo

--- kotlin/src/main/kotlin/fr/o80/aoc/day01/Day01.kt
+++ kotlin/src/main/kotlin/fr/o80/aoc/day01/Day01.kt
@@ -1,15 +1,15 @@
 package fr.o80.aoc.day01
 
-import arrow.optics.*;
-
 class Day01 {
 
-    fun parse1(input: String): List<String> =
-        input.lines()
+    fun parse1(input: String): List<List<Int>> =
+        input
+            .split("\n\n")
+            .map { it.split("\n").map { it.toInt()} }
 
-    fun part1(parsed: List<String>): String =
-        splitWhen(parsed) { it === "" }
-            .map { mapToInt(it).sum() }
+    fun part1(parsed: List<List<Int>>): String =
+        parsed
+            .map{ it.sum() }
             .max()
             .toString()
 
@@ -21,38 +21,4 @@ class Day01 {
         TODO()
     }
 
-    fun mapToInt(list: List<String>) =
-        list.map { it.toInt()}
-
-    fun <T>splitWhen (list: List<T>, predicate: (cur: T) -> Boolean): List<List<T>> {
-        val lastInListLens = lastInMatrix2<T>();
-        return when (list.size === 0) {
-            true -> listOf<List<T>>()
-            false -> list
-                .fold(listOf<List<T>>(listOf<T>())) { acc, cur ->
-                    when (predicate(cur)) {
-                        true -> lastInListLens.set(acc, listOf())
-                        false -> lastInListLens.modify(acc) {
-                            val last = it.toMutableList()
-                            last.add(cur)
-                            last.toList()
-                        }
-                    }
-                }
-        }
-    }
-
-    private fun <T>lastInMatrix2(): Lens<List<List<T>>, List<T>> =
-        Lens(
-            get = { list -> list.last() },
-            set = { list, value ->
-                val mutableList = list.toMutableList()
-                if (value.size !== 0) {
-                    mutableList.removeLast()
-                }
-                mutableList.add(value)
-                mutableList.toList()
-            }
-
-        )
 }
